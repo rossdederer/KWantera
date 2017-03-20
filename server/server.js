36dd243a9dfa529c3fd93deacb1c04762ca8a825
rossums12 <dedererross@gmail.com>
2017-03-20 09:17:23 -0400

    added logging and documenation

--- server/server.js
+++ server/server.js
@@ -5,43 +5,43 @@ var contents = "";
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongo = require('mongoskin');
-// use driver version 1.2 of mongo 
-/** 
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
-var Db = require('mongodb').Db;
-var Server = require('mongodb').Server;
-var assert      = require('assert');
-*/ 
 
+// Change this to point to your local DB. 
+// I imported my csv files into the DB called stocks 
 var URL = 'mongodb://172.16.0.9:27017/stocks';
 
+// create the DB object
 var db = mongo.db(URL, { native_parser: true });
-
-
+// this will allow us to do cross origin requests over a local network 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// API calls to send public JSON stock data. 
+// the three stocks are broken into three collections, ibm, spy, and tsla 
 app.get('/ibm',  function (req, res) {
     db.bind('ibm');
     db.ibm.find().toArray(function (err, ibm) {
         res.json(ibm);
+        console.log("Sending IBM Data");
     });
 })
 app.get('/spy', function (req, res) {
     db.bind('spy');
     db.spy.find().toArray(function (err, spy) {
         res.json(spy);  
+        console.log("Sending SPY Data");
     });
 })
 app.get('/tsla', function (req, res) {
     db.bind('tsla');
     db.tsla.find().toArray(function (err, tsla) {
         res.json(tsla);
+        console.log("Sending TSLA Data");
     });
 })
 
+// Update the port as needed. default is 3000/ 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
